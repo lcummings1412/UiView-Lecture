03e87a9deab7968da18d599f7f844d06f7cf554f
Liam Cummings <lcummings1412@stu.d214.org>
2020-11-06 14:27:26 -0600

    Initial Commit

---
+++ UiView Lecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiView Lecture
+//
+//  Created by Eileen Cummings on 11/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
